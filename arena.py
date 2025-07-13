9f04950c6151c199a44c083063a3bbc4ebb90d63
rsbof <rsbof.dev@outlook.com>
2025-07-13 12:32:17 +0900

    fix: RPCUnknownError when using UiAutomator API
    Unknown RPC error: -32001
    androidx.test.uiautomator.StaleObjectException

--- arena.py
+++ arena.py
@@ -2,17 +2,13 @@ import time
 import uiautomator2 as u2
 
 def run(args):
-    print(args)
     match args.command:
         case 'run':
             d = u2.connect()
-            skill = d(text="DELAY STRIKE III")
-            rematch = d(text='REMATCH')
+            skill = d.xpath('//*[@text="DELAY STRIKE III"]')
+            rematch = d.xpath('//*[@text="REMATCH"]')
             while True:
-                if skill.exists:
-                    skill.click()
-                elif rematch.exists:
-                    rematch.click()
-                time.sleep(0.5)
+                skill.click_exists(timeout=2)
+                rematch.click_exists(timeout=2)
         case _:
             raise ValueError(f"the command '{args.command}' does not supported.")
