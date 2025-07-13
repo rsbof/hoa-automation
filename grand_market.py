c81c1f2bf940dcf65d2ad9fdbf4689e03b307b1d
rsbof <rsbof.dev@outlook.com>
2025-07-13 09:40:19 +0900

    add: logging and error handling to grand_market.py

--- grand_market.py
+++ grand_market.py
@@ -1,10 +1,20 @@
 import time
+import logging
+from logging.handlers import RotatingFileHandler
 from typing import Optional
 import uiautomator2 as u2
 from uiautomator2 import Device, UiObject
 
 def main():
+    sh = logging.StreamHandler()
+    sh.setFormatter(logging.Formatter('%(asctime)s %(levelname)s %(message)s'))
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+    logger.addHandler(sh)
+
     d = u2.connect()
+    logging.info("Device connected: %s", d.info)
+
     d.press(0x7a)
     d(index=1, className='android.widget.TextView', text='Buy').click()
     while True:
@@ -22,33 +32,42 @@ def main():
         restock(d)
 
 def buy_items(d: Device, item_name: str = None):
-    scrollable_view = d(scrollable=True)
-    item_count = scrollable_view.info['childCount']
-    start = 14
-    end = item_count - 5
-    for i in range(start, end):
-        item = scrollable_view.child(index=i, className='android.view.View')
-        if not item.exists:
-            continue
-        name = item.child(index=2, className='android.widget.TextView')
-        if not name.exists:
-            continue
-        top = item.info['bounds']['top']
-        d.swipe(0, top, 0, 0)
-        if not item_name is None:
-            if not name.get_text() == item_name:
+    try:
+        scrollable_view = d(scrollable=True)
+        item_count = scrollable_view.info['childCount']
+        start = 14
+        end = item_count - 5
+        logging.info("Available %s items", (end - start) + 1)
+        for i in range(start, end):
+            item = scrollable_view.child(index=i, className='android.view.View')
+            if not item.exists:
                 continue
+            name = item.child(index=2, className='android.widget.TextView')
+            if not name.exists:
+                continue
+            top = item.info['bounds']['top']
+            d.swipe(0, top, 0, 0)
+            if not item_name is None:
+                if not name.get_text() == item_name:
+                    continue
 
-        buy_all = item.child(className='android.view.View', descriptionContains='BUY ALL') \
-                .child(className='android.widget.TextView', textContains='BUY ALL')
-        if buy_all.exists:
-            # hold 0.5sec
-            buy_all.swipe('right', steps=100)
+            buy_all = item.child(className='android.view.View', descriptionContains='BUY ALL') \
+                    .child(className='android.widget.TextView', textContains='BUY ALL')
+            if buy_all.exists:
+                # hold 0.5sec
+                logging.info(f"Buying item: {name.get_text()}")
+                buy_all.swipe('right', steps=100)
+    except Exception as e:
+        logging.exception("An error occurred in buy_items")
 
 def restock(d: Device):
-    scrollable_view = d(scrollable=True)
-    restock = scrollable_view.child(className='android.view.View', description='HOLD TO RESTOCK')
-    restock.swipe('right', steps=100)
+    try:
+        logging.info("Restocking items")
+        scrollable_view = d(scrollable=True)
+        restock = scrollable_view.child(className='android.view.View', description='HOLD TO RESTOCK')
+        restock.swipe('right', steps=100)
+    except Exception as e:
+        logging.exception("An error occurred in restock")
 
 if __name__ == "__main__":
     main()
